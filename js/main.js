4f14e29bc65ac5331a614d2229891bc9f75ff13d
Pete Fitton <petefitton@gmail.com>
2020-06-03 15:14:32 -0700

    Simple ex

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 console.log("👀")
+
+let url = "https://api.kanye.rest"
+
+// fetch(url)
+//     .then(response => {
+//         return response.json()
+//     })
+//     .then(data => {
+//         console.log(data)
+//         let text = document.querySelector("h1")
+//         text.innerText = data.quote
+//     })
+//     .catch(err => console.log(err))
+
+btn = document.querySelector('button')
+
+btn.addEventListener('click', () => {
+    fetch(url)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            let text = document.querySelector("h1")
+            text.innerText = data.quote
+        })
+        .catch(err => console.log(err))
+})
